1d29b9ad45ce8513a5cfecbf44679e53a6b89c5f
VikaNazarova <vikanaz98@gmail.com>
2021-01-28 05:50:11 +0400

    Updates

---
+++ precache-manifest.ec5a261dc6fb9f5da841fbc50ae5455e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3dd20ae3bfe379a8d4bc67ba0b018db",
+    "url": "/react-course-nav/index.html"
+  },
+  {
+    "revision": "062ac3d2034daef04dab",
+    "url": "/react-course-nav/static/css/main.66174328.chunk.css"
+  },
+  {
+    "revision": "216514a972551c78e7a7",
+    "url": "/react-course-nav/static/js/2.3c2b4f76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-course-nav/static/js/2.3c2b4f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062ac3d2034daef04dab",
+    "url": "/react-course-nav/static/js/main.55290d84.chunk.js"
+  },
+  {
+    "revision": "a16e3d279d472f0771a9",
+    "url": "/react-course-nav/static/js/runtime-main.3e86e55c.js"
+  }
+]);
